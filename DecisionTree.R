cfc2412cd239ec896073b22ab5d1797ea103f609
Emre Yıldırım <44210684+emreyildirim99@users.noreply.github.com>
2023-12-27 23:26:26 +0300

    KNN and Decision Tree

---
+++ DecisionTree.R
@@ -0,0 +1,58 @@
+##### Reading data from a CSV file into the 'dataset' variable
+dataset <- read.table("HeartAttack.csv", header = T, sep = ",")
+
+##### DATA PRE-PROCESSING
+
+# Converting the 'class' column in the dataset to a factor (categorical variable)
+dataset$class <- as.factor(dataset$class)
+
+# Displaying a summary of the dataset
+summary(dataset)
+
+# Creating a frequency table for the 'class' variable in the dataset
+table(dataset$class)
+
+# Creating training and testing datasets
+# install.packages("caret")
+# Loading the 'caret' package for data partitioning
+library(caret)
+
+# Creating a 70-30 split for training and test datasets
+set.seed(1)
+my_indexes <- createDataPartition(y = dataset$class, p = .70, list = FALSE)
+training <- as.data.frame(dataset[my_indexes,])
+test <- as.data.frame(dataset[-my_indexes,])
+
+##### MODELING
+# Applying C5.0 Algorithm
+# install.packages("C50")
+
+# Loading the 'C50' package for decision tree modeling
+library(C50)
+
+# Building a C5.0 decision tree model using the training data
+C50_model <- C5.0(x = training[,-9], y = training$class)
+
+# Printing the details of the C5.0 decision tree model
+print(C50_model)
+
+# Displaying a summary of the C5.0 decision tree model
+summary(C50_model)
+
+# install.packages("party")
+# install.packages("partykit")
+# Loading 'party' and 'partykit' packages for plotting
+library(party)
+library(partykit)
+
+# Plotting the C5.0 decision tree model
+plot(C50_model)
+
+# Assessing variable importance using C5imp function
+C5imp(C50_model, metric = "usage")
+
+# Making predictions on the test set using the C5.0 model
+C50_predictions <- predict(C50_model, newdata = test[,-9])
+
+# Evaluating the performance of the C5.0 model using confusion matrix
+confusionMatrix(data = C50_predictions, reference = test[[9]], dnn = c("Predictions", "Actual/Reference"), mode = "everything", positive = "positive")
---
+++ KNN.R
@@ -0,0 +1,77 @@
+##### Reading data from a CSV file into the 'dataset' variable
+dataset <- read.table("HeartAttack.csv", header = T, sep = ",")
+
+##### DATA PRE-PROCESSING
+
+# Converting the 'class' column in the dataset to a factor (categorical variable)
+dataset$class <- as.factor(dataset$class)
+
+# Displaying a summary of the dataset
+summary(dataset)
+
+# Creating a frequency table for the 'class' variable in the dataset
+table(dataset$class)
+
+# Stratified Three-way Split
+# Creating training, validation, and testing datasets
+# install.packages("caret")
+
+# Creating a stratified 60-40 split for training and the rest of the data
+set.seed(1)
+my_indexes <- caret::createDataPartition(y = dataset$class, times = 1, p = .60, list = FALSE)
+training <- as.data.frame(dataset[my_indexes,])
+the_rest <- as.data.frame(dataset[-my_indexes,])
+
+# Further splitting the rest into 50-50 for validation and test sets
+set.seed(1)
+my_indexes <- caret::createDataPartition(y = the_rest$class, times = 1, p = .50, list = FALSE)
+validation <- as.data.frame(the_rest[my_indexes,])
+test <- as.data.frame(the_rest[-my_indexes,])
+
+# Displaying frequency tables for 'class' variable in different datasets
+table(dataset$class)
+table(training$class)
+table(validation$class)
+table(test$class)
+
+# Initializing empty vectors for accuracy (acc) and precision (prec)
+acc <- NULL
+prec <- NULL
+
+# Looping through k values from 1 to 20 for kNN modeling
+for(i in 1:20){
+  ##### MODELING
+  # Applying kNN algorithm
+  
+  # Loading the 'class' library for kNN algorithm
+  # install.packages("class")
+  library(class)
+  set.seed(1)
+  
+  # Applying kNN algorithm on training and validation sets
+  (knn_predictions <- knn(train = training[, -9], test = validation[, -9], cl = training[[9]], k = i))
+  
+  # Evaluating the performance using confusion matrix
+  myConf <- confusionMatrix(data = knn_predictions, reference = validation[[9]], dnn = c("Predictions", "Actual/Reference"), mode = "everything")
+  acc[i] <- myConf$overall["Accuracy"]
+  prec[i] <- myConf$byClass["Pos Pred Value"]
+}
+
+# Plotting k-values against accuracy
+plot(x=1:length(prec), y = acc, type = "o", col = "blue", xlab = "k value", ylab = "Accuracy", main = "Performance Evaluation of kNN Classifier", ylim = c(0.5,1))
+text(x=1:length(acc), y = acc,  round(acc,2), cex=1, pos=3, col="red") 
+axis(side=2, at=c(seq(from = 0.5, to=1, by=0.1)))
+
+# Adding horizontal grid lines
+abline(h = c(seq(from = 0.5, to=1, by=0.1)), lty = 2, col = "grey")
+
+# Adding vertical grid lines
+abline(v = 1:20,  lty = 2, col = "grey")
+
+# Testing the kNN model performance on the test set with k=13
+set.seed(1)
+(knn_predictions <- knn(train = training[, -9], test = test[, -9], cl = training[[9]], k = 13))
+
+# Evaluating the performance on the test set using confusion matrix
+myConf <- confusionMatrix(data = knn_predictions, reference = test[[9]], dnn = c("Predictions", "Actual/Reference"), mode = "everything", positive = "positive")
+myConf
